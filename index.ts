5e92305bc1490bc2fe29b2ac8c2630b6ac4076a0
eyousefifar <eyousefifar@gmail.com>
2020-08-25 19:51:41 +0430

    export index

---
+++ index.ts
@@ -0,0 +1,12 @@
+export { detect } from "./src/detect-env";
+export { ErrorFactory } from "./src/errorFactory";
+export { secureRandomNumber } from "./src/random-digits";
+export { result } from "./src/result";
+export {
+  IError,
+  IResult,
+  ISecureRandomNumber,
+  TDetectPlatform,
+  TDetectX,
+  TReactNativeEngine,
+} from "./src/types";
