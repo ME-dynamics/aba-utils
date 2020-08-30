922b972be27ef38e5d445904caea88c37d6e30f0
eyousefifar <eyousefifar@gmail.com>
2020-08-30 17:44:10 +0430

    success status codes

---
+++ src/httpResult/success.ts
@@ -0,0 +1,26 @@
+import { StatusCodes } from "http-status-codes";
+import { IHttpResult, IResult } from "../types";
+
+function success<T>(args: IResult<T>, code: number): IHttpResult<T> {
+  const { payload } = args;
+  if (!payload) throw new Error("payload must be defined in ok status code");
+  return {
+    code,
+    payload,
+  };
+}
+
+export function ok<T>(args: IResult<T>): IHttpResult<T> {
+  return success(args, StatusCodes.OK);
+}
+
+export function created<T>(args: IResult<T>): IHttpResult<T> {
+  return success(args, StatusCodes.CREATED);
+}
+
+export function noContent<T>(args: IResult<T>): IHttpResult<T> {
+  return success(args, StatusCodes.NO_CONTENT);
+}
+
+
+
