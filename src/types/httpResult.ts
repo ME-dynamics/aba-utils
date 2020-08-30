c19d1f218512c306c35bd70ee81dc9a61dd4855f
eyousefifar <eyousefifar@gmail.com>
2020-08-30 17:44:22 +0430

    http result types

---
+++ src/types/httpResult.ts
@@ -0,0 +1,15 @@
+import { StatusCodes } from "http-status-codes";
+
+export interface IResult<T> {
+  payload?: T;
+  error?: string; 
+}
+ 
+
+export interface IHttpResult<T> {
+  code: StatusCodes;
+  payload?: T;
+  error?: string;
+}
+
+export type THttpVerbs = "GET" | "HEAD" | "POST" | "PUT" | "PATCH" | "DELETE";
