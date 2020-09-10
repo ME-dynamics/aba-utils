42d8571e7d40386af76bbfbf98d55d0942078b38
alireza vafaei <alirezabin2019@gmail.com>
2020-09-10 17:42:19 +0430

    requestTimeout function added to clientError

--- src/httpResult/clientError.ts
+++ src/httpResult/clientError.ts
@@ -41,3 +41,7 @@ export function notAcceptable<T>(args: IResult<T>): IHttpResult<T> {
 export function proxyAuthenticationRequired<T>(args: IResult<T>): IHttpResult<T> {
   return clientError(args, StatusCodes.PROXY_AUTHENTICATION_REQUIRED);
 }
+
+export function requestTimeout<T>(args: IResult<T>): IHttpResult<T> {
+  return clientError(args, StatusCodes.REQUEST_TIMEOUT);
+}
