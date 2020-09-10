cbb8c6027994edab132c11ef3f4b9a610bb30e9e
alireza vafaei <alirezabin2019@gmail.com>
2020-09-10 17:34:19 +0430

    Forbidden function added to clientError

--- src/httpResult/clientError.ts
+++ src/httpResult/clientError.ts
@@ -21,3 +21,7 @@ export function unauthorized<T>(args: IResult<T>): IHttpResult<T> {
 export function paymentRequired<T>(args: IResult<T>): IHttpResult<T> {
   return clientError(args, StatusCodes.PAYMENT_REQUIRED);
 }
+
+export function Forbidden<T>(args: IResult<T>): IHttpResult<T> {
+  return clientError(args, StatusCodes.FORBIDDEN);
+}
