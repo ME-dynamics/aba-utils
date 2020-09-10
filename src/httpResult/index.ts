28b995b019718e59a9e3568284a6a122cb26d5ca
alireza vafaei <alirezabin2019@gmail.com>
2020-09-10 19:55:21 +0430

    redirection functions returned in index file

--- src/httpResult/index.ts
+++ src/httpResult/index.ts
@@ -1,8 +1,9 @@
 import { created, noContent, ok, accepted, nonAuthoritativeInformation, resetContent, partialContent, multiStatus } from "./success"
 import { badRequest, unauthorized, paymentRequired, forbidden, notFound, methodNotAllowed, notAcceptable, proxyAuthenticationRequired, requestTimeout, conflict, gone, lengthRequired, preconditionFailed, requestEntityTooLarge, requestURITooLong, unsupportedMediaType, requestedRangeNotSatisfiable, expectationFailed, ImATeapot, insufficientSpaceOnResource, methodFailure, unprocessableEntity, locked, failedDependency, tooManyRequests, requestHeaderFieldsTooLarge, unavailableForLegalReasons } from "./clientError"
 import { internalServerError, notImplemented, badGateway, serviceUnavailable, gatewayTimeout, httpVersionNotSupported, insufficientStorage, networkAuthenticationRequired } from "./serverError"
+import { multipleChoices, movedPermenantly, movedTemporarily, seeOther, notModified, useProxy, temporaryRedirect, permanentRedirect } from "./redirection"
 import { IResult } from "../types";
-// TODO: return redirection, server error
+// TODO: return redirection,
 export const httpResult = {
     success: {
         ok,
@@ -13,6 +14,16 @@ export const httpResult = {
         resetContent,
         partialContent,
         multiStatus
+    },
+    redirection:{
+        multipleChoices,
+        movedPermenantly,
+        movedTemporarily,
+        seeOther,
+        notModified,
+        useProxy,
+        temporaryRedirect,
+        permanentRedirect
     },
     serverError: {
         internalServerError,
