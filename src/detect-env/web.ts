ada62cedabf55104c234d131b78c73764a965803
eyousefifar <eyousefifar@gmail.com>
2020-09-18 22:33:42 +0430

    use try catch  block for refrence error make sure always returns boolean

--- src/detect-env/web.ts
+++ src/detect-env/web.ts
@@ -1,10 +1,14 @@
 
 import { TDetectX } from '../types';
 export function detectWeb(): TDetectX {
+   try {
     if ( typeof document !== 'undefined' && typeof navigator !=='undefined' ){
 
-       return true;
-    } else {
-        return false;
-    }
+        return true;
+     } else {
+         return false;
+     }
+   } catch (error) {
+      return false; 
+   }
 }
