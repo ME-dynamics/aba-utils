4cc469e4cc68dcd04f3e1274c74a6c9b6a6e182b
eyousefifar <eyousefifar@gmail.com>
2020-09-18 22:35:31 +0430

    remove function from type section

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,9 +1,5 @@
-import { IHttpResult, IResult, IUrl } from "../types";
-
-export function result<T>(args: IResult<T>): IResult<T> {
-    return {
-        success: args.success,
-        payload: args.payload,
-        error: args.error
-    }
+export interface IResult<T> {
+  success: boolean;
+  payload?: T;
+  error?: string;
 }
