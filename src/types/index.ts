f560457632be46dcca4d689a56c35f7a9aa95f18
eyousefifar <eyousefifar@gmail.com>
2020-09-18 22:35:16 +0430

    export IResult interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,9 @@
 export { IError } from "./errorFactory";
 
-export { IHttpResult , THttpVerbs, IResult, IUrl} from "./httpResult";
+export { IHttpResult, THttpVerbs, IUrl } from "./httpResult";
 
 export { TDetectPlatform, TDetectX, TReactNativeEngine } from "./detectEnv";
 
 export { ISecureRandomNumber } from "./randomDigits";
+
+export { IResult } from "./result";
