571facb6488ca0a720f4da1fc14c207f096c9e99
eyousefifar <eyousefifar@gmail.com>
2020-09-18 22:33:14 +0430

    use try catch  block for refrence error make sure always returns boolean

--- src/detect-env/node.ts
+++ src/detect-env/node.ts
@@ -2,11 +2,16 @@
 import { TDetectX } from '../types';
 declare const process : {versions: {node: string} | undefined}
 export function detectNode(): TDetectX {
-    if(process.versions && process.versions.node ) {
-        return true
-    } else {
+    try {
+        if(process.versions && process.versions.node ) {
+            return true
+        } else {
+            return false;
+        }    
+    } catch (error) {
         return false;
     }
+    
 }
 
 
