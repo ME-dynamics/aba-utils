8f468174a9938bf6d8cca6dde50682b9403ea7d9
eyousefifar <eyousefifar@gmail.com>
2020-09-20 17:36:46 +0430

    change IResult to IHttp interface

--- src/httpResult/serverError.ts
+++ src/httpResult/serverError.ts
@@ -1,8 +1,8 @@
 import { StatusCodes } from "http-status-codes";
-import { IHttpResult, IResult } from "../types";
+import { IHttpResult, IHttp } from "../types";
 
 
-function serverError<T>(args: IResult<T>, code: number): IHttpResult<T> {
+function serverError<T>(args: IHttp<T>, code: number): IHttpResult<T> {
     const { error } = args;
     if (!error) throw new Error("error must be defined")
     return {
@@ -12,34 +12,34 @@ function serverError<T>(args: IResult<T>, code: number): IHttpResult<T> {
     };
 }
 
-export function internalServerError<T>(args: IResult<T>): IHttpResult<T> {
+export function internalServerError<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.INTERNAL_SERVER_ERROR);
 }
 
-export function notImplemented<T>(args: IResult<T>): IHttpResult<T> {
+export function notImplemented<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.NOT_IMPLEMENTED);
 }
 
-export function badGateway<T>(args: IResult<T>): IHttpResult<T> {
+export function badGateway<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.BAD_GATEWAY);
 }
 
-export function serviceUnavailable<T>(args: IResult<T>): IHttpResult<T> {
+export function serviceUnavailable<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.SERVICE_UNAVAILABLE);
 }
 
-export function gatewayTimeout<T>(args: IResult<T>): IHttpResult<T> {
+export function gatewayTimeout<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.GATEWAY_TIMEOUT);
 }
 
-export function httpVersionNotSupported<T>(args: IResult<T>): IHttpResult<T> {
+export function httpVersionNotSupported<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.HTTP_VERSION_NOT_SUPPORTED);
 }
 
-export function insufficientStorage<T>(args: IResult<T>): IHttpResult<T> {
+export function insufficientStorage<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.INSUFFICIENT_STORAGE);
 }
 
-export function networkAuthenticationRequired<T>(args: IResult<T>): IHttpResult<T> {
+export function networkAuthenticationRequired<T>(args: IHttp<T>): IHttpResult<T> {
     return serverError(args, StatusCodes.NETWORK_AUTHENTICATION_REQUIRED);
 }
