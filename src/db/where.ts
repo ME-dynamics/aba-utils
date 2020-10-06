4f3643e0695df35ce64819d7ede5e45c7dac4a68
eyousefifar <eyousefifar@gmail.com>
2020-10-06 13:34:22 +0330

    implement where conditions

--- src/db/where.ts
+++ src/db/where.ts
@@ -1,5 +1,59 @@
+import { IEqual, INotEqual, IGreaterThan, ILessThan, IN } from "../types";
+import { separator } from "./constant";
+/**
+ * will generate a equal statement
+ * like id = :id
+ * if equals self, returns argument = :argument
+ * else returns argument = valueGiven, note that
+ * this given value is hard coded to values
+ * @param args
+ */
+export function equal(args: IEqual) {
+  const { argument, equals } = args;
+  if (equals === "self") {
+    return `${argument} = :${argument}`;
+  } else {
+    return `${argument} = ${equals}`;
+  }
+}
+
+export function greaterThan(args: IGreaterThan) {
+  const { argument, greaterThan, equality } = args;
+  if (greaterThan === "self") {
+    return `${argument} >${equality ? "=" : ""} :${argument}`;
+  } else {
+    return `${argument} >${equality ? "=" : ""} ${greaterThan}`;
+  }
+}
+
+export function lessThan(args: ILessThan) {
+  const { argument, lessThan, equality } = args;
+  if (lessThan === "self") {
+    return `${argument} <${equality ? "=" : ""} :${argument}`;
+  } else {
+    return `${argument} <${equality ? "=" : ""} ${lessThan}`;
+  }
+}
+
+export function notEqual(args: INotEqual) {
+  const { argument, notEqual } = args;
+  if (notEqual === "self") {
+    return `${argument} = :${argument}`;
+  } else {
+    return `${argument} = ${notEqual}`;
+  }
+}
+
+export function IN(args: IN) {
+  const { argument, items } = args;
+  if (items === "self") {
+    return `${argument} IN :${argument}`;
+  } else {
+    return `${argument} IN (${items.join(separator)})`;
+  }
+}
 
 
-// export function equal(firstClause: string, secondClause: string) {
-    
+// export function contains(args) {
+
 // }
