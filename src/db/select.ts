a18971e1be4412682494389abad71a989cf77dfb
eyousefifar <eyousefifar@gmail.com>
2020-10-06 13:33:29 +0330

    select query generator

--- src/db/select.ts
+++ src/db/select.ts
@@ -1,15 +1,13 @@
-// import { separator } from "./constant"
-// import { ISelect } from "../types"
+import { separator } from "./constant";
+import { ISelect } from "../types";
 
+export function select(args: ISelect) {
+  const { table, version, columns, distinct, where } = args;
+  const tableName = `${table.toLowerCase()}_${version.toLowerCase()}`;
+  const clause = columns.join(separator);
+  const isDistinct = distinct ? "DISTINCT " : "";
+  return `SELECT ${isDistinct}${clause} FROM ${tableName} WHERE ${where.join(
+    " AND "
+  )};`;
+}
 
-
-
-
-// export function select(args: ISelect) {
-//     const { table, version, columns, distinct, where, orderBy } = args;
-//     const tableName = `${table.toLowerCase()}_${version.toLowerCase()}`;
-//     const clause = columns.join(separator);
-//     const isDistinct = distinct ? "DISTINCT" : "";
-//     const query = `SELECT ${isDistinct} ${clause} FROM ${tableName} WHERE ${where.join(separator)}`
-//     return query;
-// }
