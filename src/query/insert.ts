e6f5ef02158e01d49fb1ea69a3054b1c9555a69f
eyousefifar <eyousefifar@gmail.com>
2020-10-07 15:01:35 +0330

    insert query generator

---
+++ src/query/insert.ts
@@ -0,0 +1,25 @@
+import { IInsert } from "../types";
+import { separator } from "./constant";
+export function insert(args: IInsert) {
+  const { table, version, values } = args;
+  const tableName = `${table.toLowerCase()}_${version.toLowerCase()}`;
+  const columns = [];
+  const clmnValues = [];
+  for (let index = 0; index < values.length; index++) {
+    const { column, value } = values[index];
+    columns.push(column);
+    if (value === "self") {
+      clmnValues.push(`:${column}`);
+    } else if (typeof value === "string") {
+      clmnValues.push(`'${value}'`);
+    } else if (value === null) {
+      clmnValues.push("null");
+    } else {
+      clmnValues.push(value);
+    }
+  }
+  return `INSERT INTO ${tableName} (${columns.join(
+    separator
+  )}) VALUES (${clmnValues.join(separator)})`;
+}
+
