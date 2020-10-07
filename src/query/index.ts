894f999ff755747674d81234e7b8486680623eb0
eyousefifar <eyousefifar@gmail.com>
2020-10-07 19:14:22 +0330

    export new query modules

--- src/query/index.ts
+++ src/query/index.ts
@@ -1,10 +1,7 @@
 export { createTable } from "./createTable";
 export { createType } from "./createType";
 export { select } from "./select";
+export { deleteQuery } from "./delete";
+export { insert } from "./insert";
+export { update } from "./update";
 export { IN, equal, greaterThan, lessThan, notEqual } from "./where";
-
-
-
-
-
-
