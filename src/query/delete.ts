781dc7927b14d8c8d3a2766ebfb04e160661b58e
eyousefifar <eyousefifar@gmail.com>
2020-10-07 18:35:55 +0330

    delete query generator

---
+++ src/query/delete.ts
@@ -0,0 +1,11 @@
+import { IDeleteQuery } from "../types";
+import { separator, andStr } from "./constant";
+
+export function deleteQuery(args: IDeleteQuery) {
+  const { table, version, columns, where } = args;
+  const tableName = `${table.toLowerCase()}_${version.toLowerCase()}`;
+  const deleteColumns = columns ? ` ${columns.join(separator)}` : "";
+  return `DELETE${deleteColumns} FROM ${tableName} WHERE ${where.join(
+    andStr
+  )};`;
+}
