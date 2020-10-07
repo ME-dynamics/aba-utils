b810a2bb520b2949ebe152cb69bdceb07534803f
eyousefifar <eyousefifar@gmail.com>
2020-10-07 13:47:07 +0330

    remove timestamp

--- src/types/errorFactory.ts
+++ src/types/errorFactory.ts
@@ -1,10 +1,7 @@
-
 export interface IError {
-    name: string;
-    message: string;
-    detail: string | undefined;
-    path: string | undefined;
-    nativeError : Error | undefined;
-    timestamp: number | undefined;
+  name: string;
+  message: string;
+  detail: string | undefined;
+  path: string | undefined;
+  nativeError: Error | undefined;
 }
-
