9d937708414873b78377b4003abf279d55ed7741
eyousefifar <eyousefifar@gmail.com>
2020-10-07 13:47:26 +0330

    export fastify types

---
+++ src/types/http.ts
@@ -0,0 +1,6 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export type TRequest = FastifyRequest;
+// http interfaces start
+export type TReply = FastifyReply;
+// http interfaces end
