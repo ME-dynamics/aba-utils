76d232bd7e98484dea4c17e352fc538239470555
eyousefifar <eyousefifar@gmail.com>
2020-12-31 14:35:36 +0330

    add documents to http client

--- src/http-client/index.ts
+++ src/http-client/index.ts
@@ -4,7 +4,11 @@ import { fastify } from "fastify";
 
 
 
-
+/**
+ ** http_client function
+ * @param dev boolean to determine development mode
+ * @returns fastify instance
+ */
 export function http_client(dev: boolean) {
     return fastify({
         logger: dev,
