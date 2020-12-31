cad8a19a37ccffd8c251fdc1447fd3a9a402dd1a
eyousefifar <eyousefifar@gmail.com>
2020-12-31 12:18:03 +0330

    breaking: export new http result objects export all query generators as query_gen

--- index.ts
+++ index.ts
@@ -5,26 +5,17 @@ import { build_Error_Factory } from "./src/error_factory";
 export const Error_Factory = build_Error_Factory();
 export { detect_env } from "./src/detect-env";
 export { secure_random_number } from "./src/random-digits";
-export { http_result } from "./src/http-result";
+export {
+  http_result_client_error,
+  http_result_redirection,
+  http_result_server_error,
+  http_result_success,
+} from "./src/http-result";
 export { result } from "./src/result";
 
-export {
-  build_delete,
-  build_init,
-  build_select,
-  build_upsert,
-} from "./src/db-client";
+export { scylla_client } from "./src/db-client";
 
-export {
-  create_table,
-  create_type,
-  IN,
-  equal,
-  delete_query,
-  greater_than,
-  insert_query,
-  less_than,
-  not_equal,
-  select_query,
-  update_query,
-} from "./src/query";
+export * as query_gen from "./src/query";
+
+
+
