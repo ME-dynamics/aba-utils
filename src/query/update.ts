ef55305856c0299a134dc77fd589ed27e2dbf042
eyousefifar <eyousefifar@gmail.com>
2020-12-31 12:16:04 +0330

    added docs LWT support

--- src/query/update.ts
+++ src/query/update.ts
@@ -1,10 +1,16 @@
 import { separator, and_str } from "./constant";
 import { i_update_query } from "../types";
 
+/**
+ * produce query to update row in scylla db
+ * @param args
+ * @returns a query string
+ */
 export function update_query(args: i_update_query) {
-  const { table, version, values, where } = args;
+  const { table, version, values, where, lwt } = args;
   const table_name = `${table.toLowerCase()}_${version.toLowerCase()}`;
   const update_info = [];
+  const if_clause = lwt ? `IF ${lwt.join(and_str)}` : "";
   for (let index = 0; index < values.length; index++) {
     // eslint-disable-next-line security/detect-object-injection
     const { column, value } = values[index];
@@ -21,5 +27,5 @@ export function update_query(args: i_update_query) {
   }
   return `UPDATE ${table_name} SET ${update_info.join(
     separator
-  )} WHERE ${where.join(and_str)};`;
+  )} WHERE ${where.join(and_str)} ${if_clause};`;
 }
