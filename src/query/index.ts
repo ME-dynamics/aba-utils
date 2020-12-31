6596e8c5bb157652e1c18dd3427de64ff6d0cb80
eyousefifar <eyousefifar@gmail.com>
2020-12-31 12:16:23 +0330

    export all operators as single object

--- src/query/index.ts
+++ src/query/index.ts
@@ -1,7 +1,7 @@
-export { create_table } from "./create_table";
-export { create_type } from "./create_type";
+export { create_table_query } from "./create_table_query";
+export { create_type_query } from "./create_type_query";
 export { select_query } from "./select";
 export { delete_query } from "./delete";
 export { insert_query } from "./insert";
 export { update_query } from "./update";
-export { IN, equal, greater_than, less_than, not_equal } from "./where";
+export * as operators from "./operator";
