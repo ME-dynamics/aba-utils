71ab132fb0671af57fbc83a0bdb5277a10011de3
eyousefifar <eyousefifar@gmail.com>
2021-03-04 14:31:13 +0330

    add generic to fastify req, rep types

--- src/types/http.ts
+++ src/types/http.ts
@@ -1,6 +1,6 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 
-export type t_request = FastifyRequest;
+export type t_request<T> = FastifyRequest<T>;
 // http interfaces start
-export type t_reply = FastifyReply;
+export type t_reply<T> = FastifyReply<T>;
 // http interfaces end
