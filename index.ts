3aa402d580138f2eb36bf6ac7f286c1ab9217ff8
eyousefifar <eyousefifar@gmail.com>
2021-04-22 17:29:53 +0430

    export undefined to null tool

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
 import * as types from "./src/types";
 export * as types from "./src/types";
-import { buildErrorFactory } from "./src/error_factory";
+import { buildErrorFactory } from "./src/errorFactory";
 
 export const ErrorFactory = buildErrorFactory();
 export { detectEnv } from "./src/detect-env";
 
 export { result } from "./src/result";
 
-
-export { nullToUndefined } from "./src/null_to_undefined";
-
+export { nullToUndefined } from "./src/nullToUndefined";
+export { undefinedToNull } from "./src/undefinedToNull";
 
 export { time } from "./src/time";
